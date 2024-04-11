6262fdf061d2dd56f1ae6df30e3f85dfb0796bd8
Zhurbenko Maksim <zhurbenkoma97@mail.ru>
2024-04-12 01:44:53 +0300

    feat(main): добавил новый метод run и добавил аргументы командной строки

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 
+	"github.com/ChebuRashkaRF/urlshortener/cmd/config"
 	"github.com/ChebuRashkaRF/urlshortener/internal/handler"
 )
 
@@ -22,6 +25,17 @@ func ShortenerRouter() chi.Router {
 	return r
 }
 
+func run(cnf *config.Config) {
+	fmt.Println("Running server on", cnf.ServerAddress)
+	log.Fatal(http.ListenAndServe(cnf.ServerAddress, ShortenerRouter()))
+}
+
 func main() {
-	log.Fatal(http.ListenAndServe(":8080", ShortenerRouter()))
+	serverAddress := flag.String("a", ":8080", "server address")
+	baseURL := flag.String("b", "http://localhost:8080", "base URL")
+	flag.Parse()
+
+	config.Cnf = config.NewConfig(*serverAddress, *baseURL)
+
+	run(config.Cnf)
 }

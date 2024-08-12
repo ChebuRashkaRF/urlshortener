85f67cc243b96e31285db1f0fcd44c009668e655
Zhurbenko Maksim <zhurbenkoma97@mail.ru>
2024-08-12 23:53:31 +0300

    feat(main.go): добавил логику для запуска необходимого хранилища

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -17,7 +17,7 @@ func run(cnf *config.Config) error {
 		return err
 	}
 
-	logger.Log.Info("Running server on", zap.String("address", cnf.ServerAddress))
+	logger.Log.Info("Running server on", zap.String("address", cnf.ServerAddress), zap.String("storage", string(cnf.StorageType)))
 
 	return http.ListenAndServe(cnf.ServerAddress, router.NewRouter())
 }
@@ -25,27 +25,42 @@ func run(cnf *config.Config) error {
 func main() {
 	config.Cnf = config.NewConfig()
 
-	db, err := storage.NewDatabase(config.Cnf.DatabaseDSN)
-	if err != nil {
-		logger.Log.Fatal("Failed to initialize database", zap.Error(err))
-		panic(err)
-	}
-	defer db.Close()
+	var urlStorage storage.Storage
 
-	urlStorage, err := storage.NewURLStorage(config.Cnf.FileStoragePath)
-	if err != nil {
-		logger.Log.Error("Failed to initialize URL storage", zap.Error(err))
-		panic(err)
-	}
-
-	defer func() {
-		if err := urlStorage.Close(); err != nil {
-			logger.Log.Error("Error closing URLStorage", zap.Error(err))
+	switch config.Cnf.StorageType {
+	case config.DatabaseStorage:
+		db, err := storage.NewDatabaseStorage(config.Cnf.DatabaseDSN)
+		if err != nil {
+			logger.Log.Fatal("Failed to initialize database storage", zap.Error(err))
+			panic(err)
 		}
-	}()
+		defer func() {
+			if err := db.Close(); err != nil {
+				logger.Log.Error("Failed to close database", zap.Error(err))
+			}
+		}()
+		urlStorage = db
+
+	case config.FileStorage:
+		fileStorage, err := storage.NewFileStorage(config.Cnf.FileStoragePath)
+		if err != nil {
+			logger.Log.Fatal("Failed to initialize file storage", zap.Error(err))
+			panic(err)
+		}
+		defer func() {
+			if err := fileStorage.Close(); err != nil {
+				logger.Log.Error("Error closing file storage", zap.Error(err))
+			}
+		}()
+		urlStorage = fileStorage
+	case config.MemoryStorage:
+		urlStorage = storage.NewMemoryStorage()
+	default:
+		logger.Log.Fatal("Invalid storage type")
+		panic("Invalid storage type")
+	}
 
 	handler.URLStore = urlStorage
-	handler.DB = db
 
 	if err := run(config.Cnf); err != nil {
 		logger.Log.Fatal("Failed to start server", zap.Error(err))
